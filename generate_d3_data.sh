ff8fa43b8368801333f7fc8fdb299950ca301522
Xupeng Zhu <xupengz@amazon.com>
2024-10-30 20:08:01 -0400

    generating d3 demos IV

--- generate_d3_data.sh
+++ generate_d3_data.sh
@@ -9,7 +9,7 @@ cd mimicgen/scripts
 
 for ((i=0; i<n_runs; i++));
 do
-  task = ${tasks[$i]}
+  task=${tasks[$i]}
   echo "Starting generation for ${task}."
   python generate_dataset.py --config /tmp/core_configs/demo_src_${task}_task_D3.json --auto-remove-exp 2>&1 & sleep 1s
 done
