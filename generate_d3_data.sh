d00da47e517ce20df82b48a32a0494d3798e0e1b
Xupeng Zhu <xupengz@amazon.com>
2024-10-30 20:20:30 -0400

    generating d3 demos VII

--- generate_d3_data.sh
+++ generate_d3_data.sh
@@ -5,7 +5,7 @@ n_runs=${#tasks[@]}
 
 cd mimicgen/scripts
 
-#python generate_core_configs.py
+python generate_core_configs.py
 
 for ((i=0; i<n_runs; i++));
 do
@@ -16,13 +16,20 @@ done
 
 wait
 
-
-
+## in equidiffpo
+#
 #for ((i=0; i<n_runs; i++));
 #do
 #  task=${tasks[$i]}
 #  echo "Starting img voxel pcd generation for ${task}."
-#
-#  cp /tmp/core_datasets/${t}/demo_src_${t}_task_D3/demo.hdf5 data/robomimic/datasets/${t}_d3/${t}_d3.hdf5
-#  python  2>&1 & sleep 1s
+#  cp /tmp/core_datasets/${task}/demo_src_${task}_task_D3/demo.hdf5 data/robomimic/datasets/${task}_d3/${task}_d3.hdf5
+#  python equi_diffpo/scripts/dataset_states_to_obs.py --input data/robomimic/datasets/${task}_d3/${task}_d3.hdf5 \
+#  --output data/robomimic/datasets/${task}_d3/${task}_d3_pc.hdf5 --num_workers=12 && \
+#  python equi_diffpo/scripts/robomimic_dataset_conversion.py -i data/robomimic/datasets/${task}_d3/${task}_d3.hdf5 \
+#  -o data/robomimic/datasets/${task}_d3/${task}_d3_abs.hdf5 -n 12  && \
+#  python equi_diffpo/scripts/robomimic_dataset_conversion.py -i data/robomimic/datasets/${task}_d3/${task}_d3_pc.hdf5 \
+#  -o data/robomimic/datasets/${task}_d3/${task}_d3_pc_abs.hdf5 -n 12 \
+#   2>&1 & sleep 1s
 #done
+#
+#wait
