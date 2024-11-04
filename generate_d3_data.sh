7433604c139228e5dae43fc40f7e63dce9424654
xupengz <xupengz@amazon.com>
2024-11-04 14:59:00 -0500

    max_tilt = 30

--- generate_d3_data.sh
+++ generate_d3_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-tasks=(threading)
+tasks=(three_piece_assembly square coffee threading)
 n_runs=${#tasks[@]}
 
 cd mimicgen/scripts
@@ -15,21 +15,3 @@ do
 done
 
 wait
-
-## in equidiffpo
-#
-#for ((i=0; i<n_runs; i++));
-#do
-#  task=${tasks[$i]}
-#  echo "Starting img voxel pcd generation for ${task}."
-#  cp /tmp/core_datasets/${task}/demo_src_${task}_task_D3/demo.hdf5 data/robomimic/datasets/${task}_d3/${task}_d3.hdf5
-#  python equi_diffpo/scripts/dataset_states_to_obs.py --input data/robomimic/datasets/${task}_d3/${task}_d3.hdf5 \
-#  --output data/robomimic/datasets/${task}_d3/${task}_d3_pc.hdf5 --num_workers=12 && \
-#  python equi_diffpo/scripts/robomimic_dataset_conversion.py -i data/robomimic/datasets/${task}_d3/${task}_d3.hdf5 \
-#  -o data/robomimic/datasets/${task}_d3/${task}_d3_abs.hdf5 -n 12  && \
-#  python equi_diffpo/scripts/robomimic_dataset_conversion.py -i data/robomimic/datasets/${task}_d3/${task}_d3_pc.hdf5 \
-#  -o data/robomimic/datasets/${task}_d3/${task}_d3_pc_abs.hdf5 -n 12 \
-#   2>&1 & sleep 1s
-#done
-#
-#wait
